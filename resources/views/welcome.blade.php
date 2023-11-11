cf4d2a6706c4486f1fafef95fab1ff142563fd7a
unknown <theegreenweb@gmail.com>
2023-11-11 17:57:13 +0200

    remove stock slider

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2366,7 +2366,7 @@ li {
                                                                         <div class="elementor-element elementor-element-5a817888 sc_fly_static elementor-widget elementor-widget-trx_sc_button" data-id="5a817888" data-element_type="widget" data-widget_type="trx_sc_button.default">
                                                                             <div class="elementor-widget-container">
                                                                                 <div class="sc_item_button sc_button_wrap">
-                                                                                    <a href="https://auth.ecoagrozm.com/register?source=marketplace&destination=marketplace&type=farmer" class="sc_button sc_button_bordered sc_button_size_small sc_button_icon_left color_style_dark"><span class="sc_button_text"><span class="sc_button_title">
+                                                                                    <a href="https://auth.ecoagrozm.com/register?source=marketplace&destination=marketplace&type=seller" class="sc_button sc_button_bordered sc_button_size_small sc_button_icon_left color_style_dark"><span class="sc_button_text"><span class="sc_button_title">
                                                                                         Become a Seller
                                                                                         </span></span></a>
                                                                                 </div>
